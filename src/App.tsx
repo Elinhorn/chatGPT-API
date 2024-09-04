ebb86acff646083db6b53b914c66c02f83cc81fe
elinhorn <elin.horn@hyperisland.se>
2024-09-04 17:04:33 +0200

    made some simple styling and added chat history functions

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,69 @@ import { Input } from "./components/ui/input";
 
 function App() {
   const [textInput, setTextInput] = useState("");
-  const [aiOutput, setAiOutput] = useState<string | null>("");
-  const [chatHistory, setChatHistory] = useState<[]>([]);
+  const [chatHistory, setChatHistory] = useState([]);
 
   const getAI = async () => {
-    const completion = await openaitest.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: "You are a helpful assistant." },
+    let tempChat = [];
+
+    if (chatHistory.length === 0) {
+      tempChat = [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
+        },
         {
           role: "user",
           content: textInput,
         },
-      ],
+      ];
+    } else {
+      tempChat = [
+        ...chatHistory,
+        {
+          role: "user",
+          content: textInput,
+        },
+      ];
+    }
+
+    setChatHistory(tempChat);
+
+    const completion = await openaitest.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: tempChat,
     });
 
-    console.log(completion);
-    setAiOutput(completion.choices[0].message.content);
+    const updatedChat = [
+      ...tempChat,
+      {
+        role: "assistant",
+        content: completion.choices[0].message.content,
+      },
+    ];
+
+    setChatHistory(updatedChat);
+
+    setTextInput("");
   };
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center">
-        <div className="bg-slate-500 w-96 h-96">
-          <p>{aiOutput}</p>
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col gap-3 p-4 bg-slate-500 w-96 h-96">
+          {chatHistory.map((chat, index) => {
+            if (chat.role !== "system")
+              return (
+                //TODO: egen komponent
+                //TODO: stylea allt snyggt
+                <p
+                  key={index}
+                  className={chat.role === "user" ? "self-end" : "self-start"}
+                >
+                  {chat.content}
+                </p>
+              );
+          })}
         </div>
         <div className="flex">
           <Input
