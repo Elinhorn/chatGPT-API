fcd0ff9ab4688c9a389c348d141e669c6a49de15
elinhorn <elin.horn@hyperisland.se>
2024-09-16 17:03:10 +0200

    added loading component, types and buttons for local storage

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,27 @@ import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { useRef } from "react";
 import MenuBar from "./components/menuBar";
 import { ModeToggle } from "./components/mode-toggle";
+import LoadingAnswer from "./components/loadingAnswer";
+import { chatType } from "./types/dataTypes";
 
 function App() {
   const [textInput, setTextInput] = useState("");
-  const [chatHistory, setChatHistory] = useState([]);
-  const chatWindowRef = useRef(null);
+  const [chatHistory, setChatHistory] = useState<chatType[] | null>([]);
+  const chatWindowRef = useRef<HTMLDivElement>(null);
+  const [loadingAnswer, setLoadingAnswer] = useState<boolean>(false);
 
   useEffect(() => {
     scrollToBottom();
   }, [chatHistory]);
 
   const getAI = async () => {
-    console.log(textInput);
     if (!textInput.trim()) {
       return;
     }
 
     let tempChat = [];
 
-    if (chatHistory.length === 0) {
+    if (chatHistory?.length === 0) {
       tempChat = [
         {
           role: "system",
@@ -49,6 +51,7 @@ function App() {
 
     setChatHistory(tempChat);
     setTextInput("");
+    setLoadingAnswer(true);
 
     const completion = await openaitest.chat.completions.create({
       model: "gpt-3.5-turbo",
@@ -63,7 +66,9 @@ function App() {
       },
     ];
 
+    setLoadingAnswer(false);
     setChatHistory(updatedChat);
+    console.log(updatedChat);
   };
 
   const scrollToBottom = () => {
@@ -72,12 +77,26 @@ function App() {
     }
   };
 
-  const handleKeyDown = (event) => {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       event.preventDefault();
       getAI();
     }
   };
+
+  function handleSaveChat() {
+    const chat = JSON.stringify(chatHistory);
+    localStorage.setItem("chat", chat);
+    console.log("saved");
+  }
+
+  function handleLoadChat() {
+    const savedChat = localStorage.getItem("chat");
+    if (savedChat) {
+      const parsedChat = JSON.parse(savedChat);
+      setChatHistory(parsedChat);
+    }
+  }
 
   return (
     <>
@@ -85,16 +104,19 @@ function App() {
         <header className="flex w-full ">
           <ModeToggle />
           <MenuBar />
+          <Button onClick={handleSaveChat}>Save chat</Button>
+          <Button onClick={handleLoadChat}>Load old chat</Button>
         </header>
 
         <ScrollArea
           ref={chatWindowRef}
           className="flex flex-col w-full h-full gap-3 p-4 overflow-y-auto border rounded-md"
         >
-          {chatHistory.map((chat, index) => {
+          {chatHistory?.map((chat, index) => {
             if (chat.role !== "system")
               return <ChatBubble key={index} props={chat} />;
           })}
+          {loadingAnswer && <LoadingAnswer />}
         </ScrollArea>
         <div className="flex w-full gap-3 ">
           <Input
